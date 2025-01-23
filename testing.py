1cd0f6bbbbe10bf33414d3510abf733d2ab79b7f
anonymoist <66160329+alislaboratory@users.noreply.github.com>
2025-01-23 19:28:33 +1100

    Working on web interface

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ from conway import Life
 class TestingMatrix(MatrixBase):
     def __init__(self, *args, **kwargs):
         super(TestingMatrix, self).__init__(*args, **kwargs)
-        self.life = Life(32,32)
+        self.life = Life(32,16)
         self.life.randomise_grid()
 
     
@@ -14,12 +14,12 @@ class TestingMatrix(MatrixBase):
         self.offscreen_canvas = self.matrix.CreateFrameCanvas()
         while True:
             time.sleep(0.1)
-            self.life.tick()
-            for i in range(32):
+            self.life.grid = self.life.tick()
+            for i in range(16):
                 for j in range(32):
                     if self.life.grid[i][j] == 1:
-                        self.offscreen_canvas.SetPixel(i,j, 255,255,255)
-                    else: self.offscreen_canvas.SetPixel(i,j,0,0,0)
+                        self.offscreen_canvas.SetPixel(j,i, 255,255,255)
+                    else: self.offscreen_canvas.SetPixel(j,i,0,0,0)
             
             self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
 
