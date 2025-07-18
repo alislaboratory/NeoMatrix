a32213eb61a1d7f40cd6d752e0c3a248c93131bb
Ali A <ali@ashrafy.au>
2025-07-18 17:12:08 +1000

    time interval update

--- matrix_controller.py
+++ matrix_controller.py
@@ -199,6 +199,6 @@ class MatrixController:
                 for _ in range(update_interval):
                     if self.stop_event.is_set():
                         break
-                    time.sleep(0.01)
+                    time.sleep(10)
         
         self._run_in_thread(runner)
